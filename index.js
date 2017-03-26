6fe2d31b5da63eec0e448b275139e7d9875cf67c
Huiqi Zhou <blancazhou2011@gmail.com>
2017-03-25 20:51:07 -0400

    added countryfact

--- index.js
+++ index.js
@@ -24,48 +24,35 @@ function loadDataPromise(){
 function languageFacts(language){
   loadDataPromise()
     .then(function(language_data){
-      var options1 = {
+      var options = {
         threshold: 0.2,
         keys: ["name"]
         };
-      var fuse_family = new Fuse(language_data, options1)
+      var fuse_family = new Fuse(language_data, options)
       var result1 = fuse_family.search(language)
       if(result1.length>0){
         console.log(JSON.stringify(result1, null, "  "));
       }else{
         language_data.some(function(family){
-          var options2 = {
-            threshold: 0.2,
-            keys: ["name"]
-            };
-          var fuse_branch = new Fuse(family["branch"], options2)
+          var fuse_branch = new Fuse(family.branch, options)
           var result2 = fuse_branch.search(language)
           if(result2.length>0){
-            console.log("find a branch");
             console.log(JSON.stringify(result2, null, "  "));
             return true;
           }else{
             var foundALanguage = false;
-            language_data.some(function(family){
-              family["branch"].some(function(branch){
-                var options3 = {
-                  threshold: 0.2,
-                  keys: ["name"]
-                  };
-                var fuse_language = new Fuse(branch["language"], options3)
-                var result3 = fuse_language.search(language)
-                if(result3.length>0){
-                  console.log(JSON.stringify(result3, null, "  "));
-                  foundALanguage = true;
-                  return true;
-                }
-              });
-              if(foundALanguage === true){
+            family.branch.some(function(branch){
+              var fuse_language = new Fuse(branch.language, options)
+              var result3 = fuse_language.search(language)
+              if(result3.length>0){
+                console.log(JSON.stringify(result3, null, "  "));
+                foundALanguage = true;
                 return true;
               }
-              console.log("Sorry, I can't find anything. Please try again;");
             });
           }
+          return foundALanguage;
+          console.log("Sorry, I can't find anything. Please try again;");
         });
       }
       // console.log(JSON.stringify(language_data, null, '  '));
@@ -75,4 +62,79 @@ function languageFacts(language){
     });
 }
 
-languageFacts("Italian");
+// languageFacts("Yue");
+
+function CountryLanguage(name, status){
+  this.name = name;
+  this.status = status;
+}
+
+function CountryRecord(name){
+  this.name = name;
+  this.language = [];
+}
+
+CountryRecord.prototype.addLanguage = function(name, status){
+  this.language.push(new CountryLanguage(name, status));
+}
+
+function countryfacts(country){
+  loadDataPromise()
+    .then(function(language_data){
+      var options = {
+        threshold: 0.2,
+        keys: ["name"]
+        };
+      var record = new CountryRecord(country);
+      language_data.forEach(function(family){
+        var fuse_family = new Fuse(family.country, options)
+        var results1 = fuse_family.search(country)
+        results1.forEach(function(result){
+          record.addLanguage(family.name, result.status);
+        });
+      });
+      language_data.forEach(function(family){
+        family.branch.forEach(function(branch){
+          var fuse_branch = new Fuse(branch.country, options)
+          var results2 = fuse_branch.search(country)
+          results2.forEach(function(result){
+            record.addLanguage(branch.name, result.status);
+          });
+        });
+      });
+      language_data.forEach(function(family){
+        family.branch.forEach(function(branch){
+          branch.language.forEach(function(language){
+            var fuse_language = new Fuse(language.country, options)
+            var results3 = fuse_language.search(country)
+            results3.forEach(function(result){
+              record.addLanguage(language.name, result.status);
+            });
+          });
+        });
+      });
+      var importance_array = ["de facto", "national", "official", "co-official", "unofficial", "majority", "regional", "in", "significant minority", "recognized minority", "minority"]
+      function compare(a, b){
+      var status_level_a = importance_array.indexOf((a.status.split(" "))[0].toLowerCase().replace(",", ""));
+      var status_level_b = importance_array.indexOf((b.status.split(" "))[0].toLowerCase().replace(",", ""));
+      if(status_level_a === -1){
+        status_level_a = 1000;
+      }
+      if(status_level_b === -1){
+        status_level_b = 1000;
+      }
+      return status_level_a - status_level_b
+      }
+
+      record.language = record.language.sort(compare);
+      console.log(record.language);
+      if(record === undefined){
+        console.log("Sorry, I can't find anything. Please try again;")
+      }
+    })
+    .catch(function(err){
+      if(err)throw err;
+    });
+}
+
+countryfacts("china");
